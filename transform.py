115306cb9e946f425ef26b4c0ac7c475e8b9ab72
pkjayk <jayson@kadalystdesign.com>
2017-09-16 19:28:10 -0700

    Encode password before base64

--- transform.py
+++ transform.py
@@ -31,13 +31,13 @@ class transform:
 		for user in parse_json:
 			firebase_user_dict = {}
 			if user.get('_id'):
-				firebase_user_dict['localId'] = str(user.get('_id'))
+				firebase_user_dict['localId'] = str(user.get('_id')
 			if user.get('email'):
 				firebase_user_dict['email'] = str(user.get('email'))
 			if user.get('username'):
 				firebase_user_dict['displayName'] = str(user.get('username'))
 			if user.get('_hashed_password'):
-				firebase_user_dict['passwordHash'] = self.base64encode(str(user.get('_hashed_password')))
+				firebase_user_dict['passwordHash'] = self.base64encode(user.get('_hashed_password').encode())
 
 			firebase_json.append(firebase_user_dict)
 
@@ -57,7 +57,7 @@ class transform:
 	def base64encode(self, string):
 		encoded_string = ""
 		try:
-			encoded_string = base64.b64encode(string.encode('utf-8'))
+			encoded_string = base64.b64encode(string)
 		except TypeError:
 			print('Could not base64 encode hashed password')
 
