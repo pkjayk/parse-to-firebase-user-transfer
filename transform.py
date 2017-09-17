2150c0d29d72755cded163efd47a614b9159d8d6
pkjayk <jayson@kadalystdesign.com>
2017-09-16 19:47:24 -0700

    Ensure string is encoded before base64

--- transform.py
+++ transform.py
@@ -57,7 +57,7 @@ class transform:
 	def base64encode(self, string):
 		encoded_string = ""
 		try:
-			encoded_string = base64.b64encode(string)
+			encoded_string = base64.b64encode(string.encode())
 		except TypeError as e:
 			print('Could not base64 encode hashed password')
 			print(e)
